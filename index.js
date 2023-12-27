1cc8f7bbeb481f30af72bc10dd5b697e7dcd5efb
ricky-kiva <ricky.kiva@gmail.com>
2023-12-27 23:28:17 +0700

    build(fs): build FE then move FE `dist` dir. to BE

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ const generateId = () => {
     return newId
 }
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
