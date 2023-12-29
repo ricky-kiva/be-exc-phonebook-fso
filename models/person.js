eba8ad00b81115a9a7d6d87a1d37ac2b412d1ee9
ricky-kiva <ricky.kiva@gmail.com>
2023-12-29 14:46:40 +0700

    feat(model): add `Person` model from `MongoDB`

---
+++ models/person.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+mongoose.set('strictQuery', false)
+
+console.log('Connecting to', url)
+mongoose.connect(url)
+    .then(result => console.log('Connected to MongoDB'))
+    .catch(err => console.log('Error connecting to MongoDB:', err.message))
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+personSchema.set('toJSON', {
+    transform: (doc, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Person', personSchema)
