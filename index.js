11cdf99f6da8961b2ad1ca1bd1202edb6250f5a4
ricky-kiva <ricky.kiva@gmail.com>
2023-12-29 22:33:22 +0700

    feat(index): impl. PUT `Person` to `MongoDB`

--- index.js
+++ index.js
@@ -71,6 +71,20 @@ app.post('/api/persons', (req, res) => {
     person.save().then(savedPerson => res.json(savedPerson))
 })
 
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+    const id = req.params.id
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(id, person, { new: true })
+        .then(updatedPerson => res.json(updatedPerson))
+        .catch(error => next(error))
+})
+
 app.delete('/api/persons/:id', (req, res, next) => {
     const id = req.params.id
 
